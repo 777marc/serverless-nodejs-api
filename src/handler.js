44a6955d3219978cf7424c07e0e73bebcdafb6a9
Marc Mendoza <777marc@gmail.com>
2025-02-05 12:32:25 -0500

    init

---
+++ src/handler.js
@@ -0,0 +1,29 @@
+const serverless = require("serverless-http");
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res, next) => {
+  return res.status(200).json({
+    message: "Hello from root!",
+    DATABASE_URL: process.env.DATABASE_URL
+      ? process.env.DATABASE_URL
+      : "not provided",
+  });
+});
+
+app.get("/hello", (req, res, next) => {
+  return res.status(200).json({
+    message: "Hello from path!",
+  });
+});
+
+app.use((req, res, next) => {
+  return res.status(404).json({
+    error: "Not Found",
+  });
+});
+
+// server app
+// app.listen(3000, () => console.log("listening on port 3000"));
+
+exports.handler = serverless(app);
